57b0204db48c4d319c7bed26fd116264a5795f20
mr-finnie-mac <58662918+mr-finnie-mac@users.noreply.github.com>
2020-02-03 14:15:19 +0000

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -1,16 +1,20 @@
+""" 
+    EXT. Title: Led test, 12v landrover panel light
+    Author: Fin Mead, Meadeor.
+    Desc:Functional test script for LED strip (Use a relay! For the NXP HoverGames Competition//hackster.io)
+"""
+#imports
 import RPi.GPIO as GPIO
 import time
-
+#GPIO setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
+#blinking light loop
 while True:
     GPIO.output(21, GPIO.HIGH)
-    GPIO.output(24, GPIO.LOW)
     time.sleep(0.1)
     GPIO.output(21, GPIO.LOW)
-    GPIO.output(24, GPIO.HIGH)
     time.sleep(0.1)
      
     
