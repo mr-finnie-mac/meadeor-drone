2fd1d999dafb5388b83c3ff4065b4cb031cb4b2f
mr-finnie-mac <58662918+mr-finnie-mac@users.noreply.github.com>
2020-02-03 14:11:39 +0000

    Update led test.py

--- led test.py
+++ led test.py
@@ -1,16 +1,23 @@
+""" 
+    EXT. Title: Guidance & Alert programming test
+    Author: Fin Mead, Meadeor.
+    Desc: Allows access to light and audio hardware. Relay light panel and buzzer (For the NXP HoverGames Competition//hackster.io)
+"""
+#imports
 import RPi.GPIO as GPIO
 import time
-
+#setting up GPIOs
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
+#Repeating Ligh/buzzer alarm (flashing and beeping)
 while True:
     GPIO.output(21, GPIO.HIGH)
     GPIO.output(24, GPIO.LOW)
-    time.sleep(0.1)
+    time.sleep(0.1)# change sleep parameter to make it faster or slower
     GPIO.output(21, GPIO.LOW)
     GPIO.output(24, GPIO.HIGH)
-    time.sleep(0.1)
+    time.sleep(0.1)# and here
      
     
