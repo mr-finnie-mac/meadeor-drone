00413f569c782e511da803e007ba1f36f272df59
mr-finnie-mac <58662918+mr-finnie-mac@users.noreply.github.com>
2020-02-03 14:28:00 +0000

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,21 +1,28 @@
+""" 
+    EXT. Title: Posting to Twitter via RPi
+    Author: Fin Mead, Meadeor.
+    Desc:Functional test script for posting to twitter - both tweets and media (For the NXP HoverGames Competition//hackster.io)
+"""
+#imports
 from twython import Twython
-
+#importing Twitter API keys/tokens/secrets
 from auth import (
     consumer_key,
     consumer_secret,
     access_token,
     access_token_secret
 )
-
+#assigning keys/token variables
 twitter = Twython(
     consumer_key,
     consumer_secret,
     access_token,
     access_token_secret
 )
-image = open('img2.jpg', 'rb')
+#upload image/text tweet
+image = open('img.jpg', 'rb') #replace with own image//directory
 response = twitter.upload_media(media=image)
 media_id = [response['media_id']]
-message = "One of my first fire detection tests!"
+message = "One of my first fire detection tests!" #replace with your own message
 twitter.update_status(status=message, media_ids=media_id)
-print("TWEETED: " + message)
+print("TWEETED: " + message)#return print
