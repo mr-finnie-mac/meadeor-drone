d67b7eeee29a82602670e489646aee63cb2827b1
mr-finnie-mac <58662918+mr-finnie-mac@users.noreply.github.com>
2020-02-03 14:02:31 +0000

    Update pixyFireDetectionAndTweet.py

--- pixyFireDetectionAndTweet.py
+++ pixyFireDetectionAndTweet.py
@@ -1,3 +1,8 @@
+""" 
+    EXT. Title: PixyCam2 Fire detection script for Raspberry Pi - posts to twitter and controls onboard drone light/speaker.
+    Author: Fin Mead, Meadeor.
+    Desc: Main script for Raspberry Pi fire detection/tweet/alert script (For the NXP HoverGames Competition//hackster.io)
+"""
 #Pixy imports
 from __future__ import print_function
 import pixy 
@@ -11,7 +16,7 @@ import time
 #Twitter imports - twython
 from twython import Twython
 
-#Twitter imports - api auth. keys
+#Twitter imports - api auth. keys #Use your own keys and tokens!
 from auth import (
     consumer_key,
     consumer_secret,
@@ -44,13 +49,13 @@ def onboardAlert():
         i = i + 1
 
 #Drone data
-droneLoc = ("N'111 E'111")
+droneLoc = ("N'111 E'111") #! This is where I would have added in telemetry data if I had the Pi companion working properly. -See Story on hackster.io/meadeor
 
-#Pixy2 Python SWIG Fire recognition/pixy module stuff
+#Pixy2 Python SWIG Fire recognition/pixy module stuff - This is code I adapted from their demo scripts
 print("Pixy2 Python SWIG Fire recognition and tweet")
 pixy.init ()
 pixy.change_prog ("color_connected_components");
-
+#Building signature blocks...
 class Blocks (Structure):
   _fields_ = [ ("m_signature", c_uint),
     ("m_x", c_uint),
@@ -71,16 +76,17 @@ while 1:
     print('frame %3d:' % (frame))
     frame = frame + 1
     for index in range (0, count):
-      print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].m_signature, blocks[index].m_x, blocks[index].m_y, blocks[index].m_width, blocks[index].m_height))
+      print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].m_signature, blocks[index].m_x, blocks[index].m_y, blocks[index].m_width, blocks[index].m_height))#PixyCam2 return statement from demo script
       
       #Twitter API handling/posting
-      image = open('firreAlertSign.jpg', 'rb')
+      image = open('firreAlertSign.jpg', 'rb') #Fire alert image upload
       response = twitter.upload_media(media=image)
       media_id = [response['media_id']]
-      message = "(test)Potential Fire at: " + droneLoc #tweet coordinates
+      message = "(test)Potential Fire at: " + droneLoc #tweet coordinates *Fake coordinates/alert for testing!
       twitter.update_status(status=message, media_ids=media_id)
       print("TWEETED: " + message)
       
       #Physical Drone Alert
       print("Onboard audio-visual alert system is active")
       onboardAlert() #alert triggered for 10 seconds only to avoid spamming.
+   
